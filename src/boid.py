Fix avoid vel, Fix pos/vel avg formula, Add box edge type

src/boid.py
@@ -12,7 +12,7 @@ class Boid:
     init_speed: float = None
     min_speed: float = 3
     max_speed: float = 6
-    max_acc: float = 1
+    max_acc: float = 0.5
     
     view_radius: float = 40
     view_angle: float = None        # human: 220 deg, pigeon: 340 deg, owl: 110 deg
@@ -30,7 +30,11 @@ class Boid:
     
 # D-dim flock with N boids
 class Flock(Boid):
-    def __init__(self, D: int=2, N: int=1000, box_bottom=0, box_top=500, margin_bottom=100, margin_top = 100, device = torch.device("cpu"), **kwargs):
+    def __init__(self, D: int=2, N: int=1000, 
+                 box_bottom=0, box_top=500, 
+                 margin_bottom=100, margin_top = 100, 
+                 pass_through_edges=True, bouncy_edges=False,
+                 device = torch.device("cpu"), **kwargs):
         super().__init__(**kwargs)
         self.device = device
         
@@ -44,12 +48,21 @@ class Flock(Boid):
         self.bound_bottom = self.box_bottom + self.margin_bottom
         self.bound_top = self.box_top - self.margin_top
         
+        
+        self.box_upper_mat = self.box_top.unsqueeze(0).expand(N, -1)
+        self.box_lower_mat = self.box_bottom.unsqueeze(0).expand(N, -1)
+        
+        self.pass_through_edges = pass_through_edges
+        self.bouncy_edges = bouncy_edges
+        
         # init position and velocity of boids
-        self.pos = torch.rand((N, D)) * (self.box_top - self.box_bottom) + self.box_bottom
+        self.pos = torch.rand((N, D)) * (self.bound_top - self.bound_bottom) + self.bound_bottom
         if self.init_speed is None:
             self.vel = torch.randn((N, D), device=self.device) * (self.max_speed - self.min_speed) + self.min_speed
         else:
             self.vel = torch.ones((N, D), device=self.device) * self.init_speed
+        
+        
         
         
         if self.is_debug:
@@ -69,20 +82,28 @@ class Flock(Boid):
     def update(self):
         # region - init
         
+        print('-----------------Update--------------')
+        print('pos: \n', self.pos)
+        print('vel: \n', self.vel)
+        
         pos_mat = self.pos.unsqueeze(1).expand(-1, self.N, -1)              # float (N,N,D)
         vel_mat = self.vel.unsqueeze(1).expand(-1, self.N, -1)              # float (N,N,D)
-
+        # print('pos_mat: \n', pos_mat)
+        # print('vel_mat: \n', vel_mat)
+        
         # get boids position difference
         diff = pos_mat.transpose(0, 1) - pos_mat                            # float (N,N,D)
-        
+        # print('diff: \n', diff)
         # get boids distance
         sq_dist_mat = diff.pow(2).sum(dim=-1)                               # float (N,N)
+        # print('sq_dist_mat: \n', sq_dist_mat)
         # endregion
         
         # region - boids view
         # get boids in view radius
         view_mat = sq_dist_mat < self.view_radius ** 2
         view_mat.fill_diagonal_(0)                                          # bool (N,N)
+        
         # get boids in view angle
         if self.view_angle is not None:
             # if cosine similarity of pos diff and vel is greater than cosine of 
@@ -91,7 +112,8 @@ class Flock(Boid):
                 > torch.cos(self.view_angle / 2)                            # bool (N,N)
             view_mat *= view_angle_mat                                      # bool (N,N)
             
-        view_mat = view_mat.unsqueeze(-1).expand(-1, -1, self.D)            # bool (N,N,D
+        view_mat = view_mat.unsqueeze(-1).expand(-1, -1, self.D)            # bool (N,N,D)
+        # print('view_mat: \n', view_mat.int())
         # endregion
 
 
@@ -100,7 +122,7 @@ class Flock(Boid):
         # get boids in avoid radius
         avoid_mat = sq_dist_mat < self.avoid_radius ** 2
         avoid_mat.fill_diagonal_(0)                                         # bool (N,N)
-        
+        print('avoid_mat: \n', avoid_mat.int())
         # only avoid boids it can see
         if self.view_angle is not None and self.avoid_view:
             avoid_mat *= view_angle_mat
@@ -112,13 +134,19 @@ class Flock(Boid):
         avoid_mask = avoid_mat_sum != 0
         
         avoid_vel = torch.zeros((self.N, self.D), device=self.device)
-        avoid_vel[avoid_mask] = -((avoid_mat * diff).sum(dim=1))[avoid_mask] \
+        avoid_vel[avoid_mask] = ((avoid_mat * diff).sum(dim=0))[avoid_mask] \
                                     / avoid_mat_sum[avoid_mask]
-        # avoid_vel = -(avoid_mat * diff).sum(dim=1) / avoid_mat.sum(dim=1)
+        # avoid_vel[avoid_mask] = -((pos_mat * avoid_mat).sum(dim=0))[avoid_mask] \
+        #                         / avoid_mat_sum[avoid_mask]
+        # avoid_vel[avoid_mask] = -((pos_mat * avoid_mat).sum(dim=0))[avoid_mask]
         
+        # avoid_vel = -(avoid_mat * diff).sum(dim=1) / avoid_mat.sum(dim=1)
+        print('avoid_vel: \n', avoid_vel)
         # get edge avoid vector
         bottom_edge = torch.le(self.pos, self.bound_bottom)                 # bool (N,D)
         top_edge = torch.ge(self.pos, self.bound_top)
+        # print('bottom_edge: \n', bottom_edge.int())
+        # print('top_edge: \n', top_edge.int())
         # endregion
         
 
@@ -126,35 +154,47 @@ class Flock(Boid):
         # avg_pos = (pos_mat * view_mat).sum(dim=1) / view_mat.sum(dim=1)     # float (N,D)
         # avg_vel = (vel_mat * view_mat).sum(dim=1) / view_mat.sum(dim=1)
 
+        # only see boids in view and not in avoid radius
+        view_mat = view_mat & ~avoid_mat
+        
         view_mat_sum = view_mat.sum(dim=1)
+        print('view_mat_sum: \n', view_mat_sum)
         view_mask = view_mat_sum != 0
+        print('view_mask: \n', view_mask.int())
         avg_pos = torch.zeros((self.N, self.D), device=self.device)
-        avg_pos[view_mask] = ((pos_mat * view_mat).sum(dim=1))[view_mask] \
+        avg_pos[view_mask] = ((pos_mat * view_mat).sum(dim=0))[view_mask] \
                                 / view_mat_sum[view_mask]
         
         avg_vel = torch.zeros((self.N, self.D), device=self.device)
-        avg_vel[view_mask] = ((vel_mat * view_mat).sum(dim=1))[view_mask] \
+        avg_vel[view_mask] = ((vel_mat * view_mat).sum(dim=0))[view_mask] \
                                 / view_mat_sum[view_mask]
 
+        print('avg_pos: \n', avg_pos)
+        print('avg_vel: \n', avg_vel)
+        
         # region - compute velocity
+        # cohe align only if boids in view
         # Cohesion (move towards center of mass of boids in view)
-        cohe_vel = (avg_pos - self.pos) * self.cohe_factor                  # float (N,D)
-        
+        cohe_vel = (avg_pos - self.pos) * view_mask * self.cohe_factor      # float (N,D)
+        # print('cohe_vel: \n', cohe_vel)
         # Alignment (move towards average velocity of boids in view)
-        align_vel = (avg_vel - self.vel) * self.align_factor                # float (N,D)   
-        
+        align_vel = (avg_vel - self.vel) * view_mask * self.align_factor    # float (N,D)   
+        # print('align_vel: \n', align_vel)
+        # sep only if boids in avoid radius
         # Separation (move away from boids in avoid radius)
-        sep_vel = avoid_vel * self.sep_factor                               # float (N,D)        
-        
+        sep_vel = avoid_vel * avoid_mask * self.sep_factor                  # float (N,D)        
+        print('sep_vel: \n', sep_vel)
+        # TODO: custom bias
         # Bias (move towards center of box)
         bias_vel = ((self.box_bottom + self.box_top) / 2 - self.pos) * self.bias_factor
+        print('bias_vel: \n', bias_vel)
         
         # Edge (move away from edges)
         # # move up if bottom edge, move down if top edge
         edge_vel = bottom_edge * self.edge_factor - top_edge * self.edge_factor
-        
+        print('edge_vel: \n', edge_vel)
         # get new velocity
-        sum_d_vel = cohe_vel + align_vel + sep_vel + edge_vel + bias_vel                # d_vel from boids rules
+        sum_d_vel = cohe_vel + align_vel + sep_vel + bias_vel + edge_vel                # d_vel from boids rules
         # endregion
         
         # region - limit speed and acceleration
@@ -171,7 +211,8 @@ class Flock(Boid):
         sum_vel = self.vel + acc
         vel_mag = sum_vel.pow(2).sum(dim=-1).sqrt()                         # float (N)
         vel_clipped = torch.clamp(vel_mag, min=self.min_speed, max=self.max_speed)
-        sum_vel = sum_vel/vel_mag.unsqueeze(-1) * vel_clipped.unsqueeze(-1)
+        sum_vel = torch.nn.functional.normalize(sum_vel, p=2, dim=-1) * vel_clipped.unsqueeze(-1)    # float (N,D)
+        # sum_vel = sum_vel/vel_mag.unsqueeze(-1) * vel_clipped.unsqueeze(-1)
         # endregion
         
         # region - update position and velocity
@@ -179,9 +220,22 @@ class Flock(Boid):
         self.pos = self.pos + self.vel
         
         # pass through edges
-        self.pos = torch.where(self.pos < self.box_bottom, self.box_top, self.pos)
-        self.pos = torch.where(self.pos > self.box_top, self.box_bottom, self.pos)
+        # self.pos = torch.where(self.pos < self.box_bottom, self.box_top, self.pos)
+        # self.pos = torch.where(self.pos > self.box_top, self.box_bottom, self.pos)
         
+        if self.pass_through_edges:
+            self.pos = torch.where(self.pos < self.box_lower_mat, self.box_upper_mat, self.pos)
+            self.pos = torch.where(self.pos > self.box_upper_mat, self.box_lower_mat, self.pos)
+        # solid edges
+        else: 
+            # flip velocity if boid hits edge
+            if self.bouncy_edges:
+                self.vel = torch.where(self.pos < self.box_lower_mat, -self.vel, self.vel)
+                self.vel = torch.where(self.pos > self.box_upper_mat, -self.vel, self.vel)
+            
+            self.pos = torch.maximum(self.pos, self.box_lower_mat)
+            self.pos = torch.minimum(self.pos, self.box_upper_mat)
+            
         
         # self.pos = torch.max(self.pos, self.box_bottom)
         # self.pos = torch.min(self.pos, self.box_top)

src/simulation.py
@@ -12,16 +12,37 @@ from boid import Flock
 pygame.init()
 clock = pygame.time.Clock()
 
-
-fps = 1
-
 # Set the window dimensions
-world_size = 2000
-window_size = 1000
+world_size = 500
+window_size = 500
+margin_top = margin_bottom = 150
+
+
 ww_ratio = window_size / world_size
 window_size = (window_size, window_size)
+my_font = pygame.font.SysFont("monospace", 16)
 
+is_draw_range = 1
+is_draw_number = 1
+triangle_size = 10
+fps = 30 
 
+# set boids properties
+N = 50
+
+init_speed=None
+
+view_radius = 40
+avoid_radius = 15
+
+sep_factor = 0.05        # avoidfactor
+align_factor = 0.05      # matchingfactor
+cohe_factor = 0.0005     # centeringfactor
+bias_factor = 0.00
+edge_factor = 0.2       # turnfactor
+
+pass_through_edges = 0
+bouncy_edges = 0
 
 # Create a window
 screen = pygame.display.set_mode(window_size)
@@ -31,11 +52,18 @@ pygame.display.set_caption("Boids Simulation")
 bg_color = (0, 0, 0)
 color = (255, 255, 255)
 
+# seed = 1337
+# torch.manual_seed(seed)
+
 
 device = torch.device("cpu")
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-flock_1 = Flock(D=2, N=100, box_top=world_size, 
-                bias_factor=0,
+flock_1 = Flock(D=2, N=N, box_top=world_size, 
+                sep_factor=sep_factor, align_factor=align_factor, cohe_factor=cohe_factor, 
+                bias_factor=bias_factor, edge_factor=edge_factor, 
+                pass_through_edges=pass_through_edges, bouncy_edges=bouncy_edges,
+                init_speed=init_speed, 
+                margin_bottom=margin_bottom, margin_top=margin_top,
                 device=device)
 
 
@@ -79,18 +107,44 @@ def draw_flock_range(screen, position, avoid_radius, view_radius, width=1):
     pygame.draw.circle(screen, (255, 0, 0), (int(position[0]), int(position[1])), int(avoid_radius), width)
     pygame.draw.circle(screen, (0, 255, 0), (int(position[0]), int(position[1])), int(view_radius), width)
 
-
+def draw_text(screen, text, position, color = None, triangle_size = None):
+    if color is None:
+        color = (0, 0, 255)
+    label = my_font.render(text, 1, color)
+    
+    if triangle_size is None:
+        pos = (int(position[0]), int(position[1]))
+    else:
+        pos = (int(position[0]-triangle_size/2), int(position[1]-triangle_size/2))
+    screen.blit(label, pos)
+    
+def draw_margin(screen, top_margin, bottom_margin, color = None):
+    if color is None:
+        color = (255, 0, 0)
+    pygame.draw.rect(screen, color, ((bottom_margin[0]), bottom_margin[1], top_margin[0]-bottom_margin[0], top_margin[1]-bottom_margin[1]), width=1)
 
 running = True
 
-print(flock_1.pos, flock_1.vel)
 
+top_margin = map_point_to_screen(flock_1.bound_top, flock_1).astype(int)
+bottom_margin = map_point_to_screen(flock_1.bound_bottom, flock_1).astype(int)
+
+print(top_margin, bottom_margin)
+
+is_paused = False
 while running:
     clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+            
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_paused = not is_paused
+    
+    
+    if is_paused:
+        continue
+    
     # Clear the screen
     screen.fill(bg_color)
     
@@ -98,11 +152,21 @@ while running:
     # game_vel = map_vector_to_screen(flock_1.vel, flock_1)
     game_vel_normed = map_vector_to_screen(torch.nn.functional.normalize(flock_1.vel, p=2, dim=-1), flock_1)
 
+    # Draw origin and margin
+    origin = torch.tensor([0, 0], device=flock_1.device)
+    origin = map_point_to_screen(origin, flock_1)
+    draw_text(screen, "origin", origin, color=(255, 0, 0))
+    pygame.draw.circle(screen, (255, 0, 0), (int(origin[0]), int(origin[1])), 5)
+    
+    draw_margin(screen, top_margin, bottom_margin, color=(255, 255, 255))
     
     for i in range(flock_1.N):
         # pygame.draw.circle(screen, color, (int(game_pos[i][0]), int(game_pos[i][1])), 5)
-        draw_triangles(screen, color, game_pos[i], game_vel_normed[i], 10)
-        draw_flock_range(screen, game_pos[i], flock_1.avoid_radius * ww_ratio, flock_1.view_radius * ww_ratio, width=1)
+        draw_triangles(screen, color, game_pos[i], game_vel_normed[i], triangle_size)
+        if is_draw_range:
+            draw_flock_range(screen, game_pos[i], flock_1.avoid_radius * ww_ratio, flock_1.view_radius * ww_ratio, width=1)
+        if is_draw_number:
+            draw_text(screen, f"{i}", (game_pos[i][0], game_pos[i][1]), triangle_size = triangle_size)
     # draw_triangles(screen, color, flock_1.pos, flock_1.vel, 5, flock_1.N)
     
     